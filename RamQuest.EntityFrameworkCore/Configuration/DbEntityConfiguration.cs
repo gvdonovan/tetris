cd3557470b5bcfc2a759e33d65b3d3e2e4ee3022
Greg Donovan <gdonovan@Ramquest.com>
2017-02-01 17:42:06 -0600

    adding projects

---
+++ RamQuest.EntityFrameworkCore/Configuration/DbEntityConfiguration.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace RamQuest.EntityFrameworkCore.Configuration
+{
+    abstract class DbEntityConfiguration<TEntity> where TEntity : class
+    {
+        public abstract void Configure(EntityTypeBuilder<TEntity> entity);
+    }
+}
---
+++ RamQuest.EntityFrameworkCore/Extensions/ModelBuilderExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using RamQuest.EntityFrameworkCore.Configuration;
+
+namespace RamQuest.EntityFrameworkCore.Extensions
+{
+    static class ModelBuilderExtensions
+    {
+        public static void AddConfiguration<TEntity>(this ModelBuilder modelBuilder,
+            DbEntityConfiguration<TEntity> entityConfiguration) where TEntity : class
+        {
+            modelBuilder.Entity<TEntity>(entityConfiguration.Configure);
+        }
+    }
+}
