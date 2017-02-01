135edb89481f3f9b5a60fe35f6ff17da82709b8d
Greg Donovan <gdonovan@Ramquest.com>
2017-02-01 17:43:27 -0600

    removed unused using statments

--- RamQuest.EntityFrameworkCore/Extensions/ModelBuilderExtensions.cs
+++ RamQuest.EntityFrameworkCore/Extensions/ModelBuilderExtensions.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using RamQuest.EntityFrameworkCore.Configuration;
 
 namespace RamQuest.EntityFrameworkCore.Extensions
